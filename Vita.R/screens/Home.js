acd0defc37ca6a0825078e59cf58d42401203f5d
3mptySpac3 <thefantasticalproject@gmail.com>
2024-01-16 22:57:18 -0700

    we swimming now

--- Vita.R/screens/Home.js
+++ Vita.R/screens/Home.js
@@ -1,11 +1,44 @@
-
-import { SafeAreaView, Text, View  } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { Button, Icon, Layout } from '@ui-kitten/components';
 
 export default function Home() {
+  const pulseIconRef = useRef();
+
+  useEffect(() => {
+    pulseIconRef.current.startAnimation();
+  }, []);
+
+  const renderPulseIcon = (props) => (
+    <Icon
+      {...props}
+      ref={pulseIconRef}
+      animation='pulse'
+      name='activity'
+    />
+  );
+
   return (
-    <SafeAreaView >
-      <Text>Home!Screen</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        style={styles.button}
+        accessoryLeft={renderPulseIcon}
+        onPress={() => pulseIconRef.current.startAnimation()}
+      >
+        PULSE
+      </Button>
     </SafeAreaView>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    margin: 8,
+  },
+});
