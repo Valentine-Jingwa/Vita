267a915201d61eaf8351e072848022e5ca5bf612
3mptySpac3 <thefantasticalproject@gmail.com>
2024-01-16 21:13:46 -0700

    Start of navigation.js

---
+++ Vita.R/Navigation.js
@@ -0,0 +1,9 @@
+import { NavigationContainer } from "@react-navigation/native";
+
+export default function Navigation(){
+  return (
+    <NavigationContainer>
+      
+    </NavigationContainer>
+  )
+}
