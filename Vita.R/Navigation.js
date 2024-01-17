486376dd07685f7effeb59aaad0a7882b864b773
3mptySpac3 <thefantasticalproject@gmail.com>
2024-01-16 21:32:02 -0700

    Nav

--- Vita.R/Navigation.js
+++ Vita.R/Navigation.js
@@ -1,9 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+//Bottom Tab Navigator
+const Tab = createBottomTabNavigator();
+
+function BottomTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+    </Tab.Navigator>
+  );
+}
 
 export default function Navigation(){
   return (
     <NavigationContainer>
-      
+      <Home />
     </NavigationContainer>
   )
 }
---
+++ Vita.R/screens/Home.js
@@ -0,0 +1,11 @@
+
+import { SafeAreaView, Text, View  } from 'react-native';
+
+export default function Home() {
+  return (
+    <SafeAreaView >
+      <Text>Home! Testing this out</Text>
+    </SafeAreaView>
+  );
+}
+
