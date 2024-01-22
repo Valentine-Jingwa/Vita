0021f7a27bbc4fd1a210984f86f2f8deb9cca7e2
Valentine-Jingwa <119522550+Valentine-Jingwa@users.noreply.github.com>
2024-01-22 10:30:53 -0700

    none

--- screens/profile.js
+++ screens/profile.js
@@ -7,4 +7,4 @@ export default function Profile() {
     </SafeAreaView>
   );
 }
-//for the profile page i want a profile pricture rounded which has a blue border around it. The profile picture should be in the middle of the screen. 
+ 
