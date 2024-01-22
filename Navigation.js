90c907b7738985f40e457a21e880f2b17d94f502
Valentine-Jingwa <119522550+Valentine-Jingwa@users.noreply.github.com>
2024-01-21 23:39:39 -0700

    Minor adjustments

--- Navigation.js
+++ Navigation.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import Home from "./screens/Home";
 import Profile from "./screens/Profile";
 import Viewing from "./screens/Viewing";
+import AddData from "./screens/AddData";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 //Bottom Tab Navigator
@@ -10,8 +11,9 @@ const Tab = createBottomTabNavigator();
 function BottomTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Profile" component={Profile} />
       <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="AddData" component={AddData} />
       <Tab.Screen name="Viewing" component={Viewing} />
     </Tab.Navigator>
   );
---
+++ screens/AddData.js
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text, View  } from 'react-native';
+
+export default function AddData() {
+  return (
+    <SafeAreaView >
+      <Text>Profile! Screen</Text>
+    </SafeAreaView>
+  );
+}
