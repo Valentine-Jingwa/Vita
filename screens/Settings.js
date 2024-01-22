e0b990ee483a71027f2661be6eb11b991a21102b
3mptySpac3 <thefantasticalproject@gmail.com>
2024-01-22 10:48:49 -0700

    Delete Settings.js

--- screens/Settings.js
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView, Text, View  } from 'react-native';
-
-export default function Settings() {
-  return (
-    <SafeAreaView >
-      <Text>Settings! Screen</Text>
-    </SafeAreaView>
-  );
-}
