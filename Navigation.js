a6af696698f697bd3d3f36c2e3e0c51d7830221f
Valentine-Jingwa <119522550+Valentine-Jingwa@users.noreply.github.com>
2024-01-22 11:36:05 -0700

    Profile Basic

--- Navigation.js
+++ Navigation.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from "./screens/Home";
-import Profile from "./screens/Profile";
+import Profile from "./screens/profile";
 import Viewing from "./screens/Viewing";
 import AddDataOptions from "./screens/AddDataOptions"; // Your initial AddData screen is now AddDataOptions
 import DataCategory from "./screens/DataCategory"; // The new screen you'll create
