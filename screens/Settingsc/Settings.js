a12f17091aa56ebcce5b616270ba0e9a940d494d
Valentine-Jingwa <119522550+Valentine-Jingwa@users.noreply.github.com>
2024-03-14 18:54:40 -0600

    Seet

--- screens/Settingsc/Settings.js
+++ screens/Settingsc/Settings.js
@@ -13,7 +13,7 @@ export default function Settings({ navigation }) {
 
   const handleClearStorage = async () => {
     await DataStorage.Clear();
-    setModalVisible(false);
+    setModalVisible(false);``
   };
 
   const handleLogout = async () => {
