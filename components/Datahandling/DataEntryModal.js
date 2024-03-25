118ebe67094d469f22584e5dc683edfa1690b4df
Valentine-Jingwa <119522550+Valentine-Jingwa@users.noreply.github.com>
2024-03-25 12:56:43 -0600

    Add New Form design

--- components/Datahandling/DataEntryModal.js
+++ components/Datahandling/DataEntryModal.js
@@ -1,66 +1,87 @@
 // DataEntryModal.js
 
 import React, { useState } from 'react';
-import { Modal, View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { Modal, View, Text, TextInput, TouchableOpacity, StyleSheet, Animated, Alert } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
-import { subcategories } from '../DataList';
+import { AntDesign } from '@expo/vector-icons';
+import { subcategories } from '../DataList'; // Make sure this import path is correct
 
 const DataEntryModal = ({ isVisible, onClose, subcategory, onSave }) => {
-  if (!subcategory) return null; // Prevents the modal from rendering if no subcategory is selected
+  if (!subcategory) return null;
 
   const [inputValue, setInputValue] = useState('');
   const [selectedUnit, setSelectedUnit] = useState(subcategory.dunit || '');
+  const [notificationOpacity] = useState(new Animated.Value(0));
 
-  const validateAndSave = (shouldCloseAfterSave) => {
-    const trimmedInput = inputValue.trim();
-    if (!trimmedInput || !selectedUnit) {
-      Alert.alert('Invalid data', 'Please ensure all fields are correctly filled.');
+  const validateAndSave = () => {
+    const value = Number(inputValue.trim());
+    if (isNaN(value) || value < 0 || value > 999) {
+      Alert.alert('Invalid data', 'Please enter a valid number (0-999)');
       return;
     }
-    onSave(subcategory.id, trimmedInput, selectedUnit, subcategory.subcategory, subcategory.categoryname);
+
+    onSave(subcategory.id, value.toString(), selectedUnit, subcategory.subcategory, subcategory.categoryname);
     setInputValue('');
-    if (shouldCloseAfterSave) onClose();
+    onClose();
+    showNotification();
   };
 
-  const unitsArray = Array.isArray(subcategory.units) ? subcategory.units : [subcategory.units];
-  const handleSave = () => validateAndSave(true);
-  const handleSaveAndAddMore = () => validateAndSave(false);
+  const showNotification = () => {
+    Animated.sequence([
+      Animated.timing(notificationOpacity, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+      Animated.delay(3000),
+      Animated.timing(notificationOpacity, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+    ]).start();
+  };
 
   return (
     <Modal visible={isVisible} animationType="slide" onRequestClose={onClose} transparent={true}>
       <View style={styles.modalOverlay}>
         <View style={styles.modalView}>
           <TouchableOpacity style={styles.closeButton} onPress={onClose}>
-            <Text style={styles.closeButtonText}>Exit</Text>
+            <AntDesign name="close" size={24} color="black" />
           </TouchableOpacity>
           <Text style={styles.subcategoryTitle}>{subcategory.subcategory}</Text>
           <TextInput
             style={styles.input}
             value={inputValue}
-            onChangeText={setInputValue}
-            keyboardType="default"
-            placeholder="Enter value"
+            onChangeText={text => setInputValue(text.replace(/[^0-9]/g, ''))}
+            keyboardType="numeric"
+            maxLength={3}
+            placeholder="Enter value (0-999)"
           />
-          {unitsArray.length > 0 && (
+          {subcategory.units && (
             <Picker selectedValue={selectedUnit} onValueChange={setSelectedUnit} style={styles.picker}>
-              {unitsArray.map((unit, index) => (
+              {subcategory.units.map((unit, index) => (
                 <Picker.Item key={index} label={unit} value={unit} />
               ))}
             </Picker>
           )}
           <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
+            <TouchableOpacity style={styles.saveButton} onPress={validateAndSave}>
               <Text style={styles.buttonText}>Save</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.saveAddMoreButton} onPress={handleSaveAndAddMore}>
+            <TouchableOpacity style={styles.saveAddMoreButton} onPress={validateAndSave}>
               <Text style={styles.buttonText}>Save and Add More</Text>
             </TouchableOpacity>
           </View>
+          <Animated.View style={[styles.notification, { opacity: notificationOpacity }]}>
+            <Text style={styles.notificationText}>Data saved successfully!</Text>
+          </Animated.View>
         </View>
       </View>
     </Modal>
   );
 };
+
 
 const styles = StyleSheet.create({
   modalOverlay: {
